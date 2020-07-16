4139d53aecd30b4c182ec3339d73eb91a681a409
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2020-07-16 10:34:11 +0200

    added cplx magnitude ip

---
+++ ip/cplx_magnitude/sim/testbench.vhd
@@ -0,0 +1,70 @@
+library ieee;
+use     ieee.numeric_std.all;
+use     ieee.std_logic_1164.all;
+
+entity testbench is
+end testbench;
+
+architecture rtl of testbench is
+
+	signal clk: std_logic := '0';
+	
+	signal cplx_ready, cplx_valid: std_logic := '0';
+	signal cplx_data: std_logic_vector(15 downto 0) := x"0101"; 
+	signal cplx_last: std_logic := '0';
+
+	signal last_count: natural range 0 to 7 := 0;
+
+	signal magnitude_valid: std_logic;
+	signal magnitude_data: std_logic_vector(15 downto 0);
+	signal magnitude_last: std_logic;
+
+begin
+	
+	clk <= not(clk) after 5.0 ns;
+
+	process (clk)
+	begin
+	if rising_edge (clk) then
+		cplx_valid <= not(cplx_valid);
+		cplx_last <= '0';
+		
+		if cplx_valid = '0' then
+			if last_count < 7 then
+				last_count <= last_count+1;
+			else
+				last_count <= 0;
+				cplx_last <= '1';
+			end if;
+		end if;
+
+		if cplx_valid = '1' then
+			cplx_data(7 downto 0) <= std_logic_vector(
+				unsigned(cplx_data(7 downto 0))+1
+			);
+			
+			cplx_data(15 downto 8) <= std_logic_vector(
+				unsigned(cplx_data(15 downto 8))+1
+			);
+
+		end if;
+	end if;
+	end process;
+
+	dut: entity work.cplx_magnitude
+	generic map (
+		G_DATA_WIDTH => 8
+	) port map (
+		clk => clk,
+		-- DIN
+		cplx_ready => cplx_ready,
+		cplx_valid => cplx_valid,
+		cplx_data => cplx_data,
+		cplx_last => cplx_last,
+		-- DOUT
+		magnitude_valid => magnitude_valid,
+		magnitude_data => magnitude_data,
+		magnitude_last => magnitude_last
+	);
+
+end rtl;
--- ip/cplx_magnitude/src/cplx_magnitude.vhd
+++ ip/cplx_magnitude/src/cplx_magnitude.vhd
@@ -1,4 +1,5 @@
 library ieee;
+use     ieee.numeric_std.all;
 use     ieee.std_logic_1164.all;
 
 entity cplx_magnitude is
@@ -11,19 +12,23 @@ port (
 	cplx_ready: out std_logic;
 	cplx_valid: in std_logic;
 	cplx_data: in std_logic_vector(2*G_DATA_WIDTH-1 downto 0);
+	cplx_last: in std_logic;
 	-- magnitude/out
 	magnitude_valid: out std_logic;
-	magnitude_data: out std_logic_vector(2*G_DATA_WIDTH+1-1 downto 0)
+	magnitude_data: out std_logic_vector(2*G_DATA_WIDTH-1 downto 0);
+	magnitude_last: out std_logic
 );
 end cplx_magnitude;
 
 architecture rtl of cplx_magnitude is
 
-	signal ab_reg_valid: std_logic;
-	signal a_reg, b_reg: std_logic_vector(2*G_DATA_WIDTH-1 downto 0);
+	signal ab_reg_valid: std_logic := '0';
+	signal a_reg, b_reg: std_logic_vector(2*G_DATA_WIDTH-1 downto 0) := (others => '0');
+	signal ab_reg_last: std_logic := '0';
 
-	signal c_reg_valid: std_logic;
-	signal c_reg: std_logic_vector(2*G_DATA_WIDTH+1-1 downto 0);
+	signal c_reg_valid: std_logic := '0';
+	signal c_reg: std_logic_vector(2*G_DATA_WIDTH-1 downto 0) := (others => '0');
+	signal c_reg_last: std_logic := '0';
 begin
 
 	cplx_ready <= '1';
@@ -32,19 +37,24 @@ begin
 	begin
 	if rising_edge (clk) then
 		ab_reg_valid <= '0';
+		ab_reg_last <= '0';
 
 		if cplx_valid = '1' then
 			ab_reg_valid <= '1';
 			
 			a_reg <= std_logic_vector(
-				signed(cplx_data(2*G_DATA_WIDTH-1 downto G_DATA_WIDTH)
-				* signed(cplx_data(2*G_DATA_WIDTH-1 downto G_DATA_WIDTH)
+				signed(cplx_data(2*G_DATA_WIDTH-1 downto G_DATA_WIDTH))
+				* signed(cplx_data(2*G_DATA_WIDTH-1 downto G_DATA_WIDTH))
 			);
 			
 			b_reg <= std_logic_vector(
-				signed(cplx_data(G_DATA_WIDTH-1 downto 0)
-				* signed(cplx_data(G_DATA_WIDTH-1 downto 0)
+				signed(cplx_data(G_DATA_WIDTH-1 downto 0))
+				* signed(cplx_data(G_DATA_WIDTH-1 downto 0))
 			);
+
+			if cplx_last = '1' then
+				ab_reg_last <= '1';
+			end if;
 
 		end if;
 	end if;
@@ -54,14 +64,20 @@ begin
 	begin
 	if rising_edge (clk) then
 		c_reg_valid <= '0';
+		c_reg_last <= '0';
 		if ab_reg_valid = '1' then
 			c_reg_valid <= '1';
 			c_reg <= std_logic_vector(signed(a_reg)+signed(b_reg));
+
+			if ab_reg_last = '1' then
+				c_reg_last <= '1';
+			end if;
 		end if;
 	end if;
 	end process;
 
 	magnitude_valid <= c_reg_valid;
 	magnitude_data <= c_reg;
+	magnitude_last <= c_reg_last;
 
 end rtl;
