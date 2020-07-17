5c2d0c6fa0985be094375315b47a32532de1e817
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2020-07-17 22:03:22 +0200

    working on .wav data

--- python/audio/wav.py
+++ python/audio/wav.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Wav (object):
 
 	def __init__ (self, fp):
@@ -6,55 +8,69 @@ class Wav (object):
 
 		fileTypeBlocID = content[0:4]
 		if fileTypeBlocID != b'RIFF':
-			print('RIFF TypeBloc ID error!')
+			raise RuntimeError('RIFF TypeBloc ID error!')
 
-		self.fileSize = 0
-		# fileSize - 8 (RIFF header size)
-		for i in range (0, len(content[4:8])):
-			self.fileSize += int(content[4:8][i]) << (8*i) 
-		print(self.fileSize)
+		self.fileSize = self._parseIntegerValue(content[4:8])
 
 		fileFormatID = content[8:12]
 		if fileFormatID != b'WAVE':
-			print('Corrupted .wav file!')
+			raise RuntimeError('Corrupted .wav file!')
 
 		formatBlocID = content[12:16]
 		if formatBlocID != b'fmt ':
-			print('Corrupted .wav format description')
+			raise RuntimeError('Corrupted .wav format description')
 
-		self.blocSize = 0
-		# bloc size -16
-		for i in range (0, len(content[16:20])):
-			self.blocSize += int(content[16:20][i]) << (8*i)
-		print(self.blocSize)
+		self.blocSize = self._parseIntegerValue(content[16:20])
 		
-
 		audioFormat = int(content[20])
 		if audioFormat == 1:
 			self.audioFormat = 'PCM'
 		else:
 			self.audioFormat = 'Unknown'
-		print(self.audioFormat)
-		# drop[22]
-
-		self.nbChannels = 0
-		for i in range (0, len(content[22:24])):
-			self.nbChannels += int(content[22:24][i])
-		print(self.nbChannels)
-
-		self.sampleRate = 0
-		for i in range (0, len(content[24:28])):
-			self.sampleRate += int(content[24:28][i])
-		print('Sample rate: ', self.sampleRate, ' Hz')
-
+		
+		self.nbChannels = self._parseIntegerValue(content[22:24])
+		self.sampleRate = self._parseIntegerValue(content[24:28])
 		bytesPerSec = content[28:32]
 		bytesPerBloc = content[32:34]
+		self.bitsPerSample = self._parseIntegerValue(content[34:36])
 
-		self.bitsPerSample = 0
-		for i in range (0, len(content[34:36])):
-			self.bitsPerSample += int(content[34:36][i])
-		print('Bits per sample: ', self.bitsPerSample)
+		dataBlocId = content[36:40]
+		if dataBlocId != b'data':
+			raise RuntimeError('Corrupted .wav data description')
 
+		self.dataSize = self._parseIntegerValue(content[40:44])
+
+		# parsing data
+		nbSymbols = int(self.dataSize /8 /self.bitsPerSample /self.nbChannels)
+		self.data = np.ones((self.nbChannels,nbSymbols))
+		offset = 40
+		bytesPerSymbols = self.bitsPerSample //8 
+		for i in range (0, self.data.shape[1]):
+			for j in range (0, self.nbChannels):
+				c = content[offset:offset+bytesPerSymbols]
+				print(offset, offset+bytesPerSymbols)
+				offset += bytesPerSymbols
+				self.data[j][i] = self._parseIntegerValue(c)
+
+		# normalize data to [-1:1] signed data
+		norm = pow(2,self.bitsPerSample) -1
+		self.data /= norm 
+
+	def numberOfSymbols (self):
+		return self.data.shape[1]
+
+	def getData (self):
+		"""
+		Returns data for all channels
+		"""
+		return self.data
+	
+	def getChannelData (self, channel):
+		"""
+		Returns data for given channel
+		"""
+		return self.data[channel]
+			
 	def _read_raw (self, fp):
 		"""
 		Returns raw binary content
@@ -62,11 +78,25 @@ class Wav (object):
 		"""
 		with open(fp, 'rb') as fd:
 			return fd.read()
+
+	def __str__ (self):
+		string  = 'Audio Format: {:s}\n'.format(self.audioFormat)
+		string += 'Sample Rate: {:d} Hz\n'.format(self.sampleRate)
+		string += 'Nb Channels: {:d}\n'.format(self.nbChannels)
+		string += 'Bits per sample: {:d}\n'.format(self.bitsPerSample)
+		string += 'File size: {:d} bytes\n'.format(self.fileSize)
+		string += 'Bloc size: {:d}\n'.format(self.blocSize)
+		string += 'Data size: {:d} bytes\n'.format(self.dataSize)
+		return string
+
+	def _parseIntegerValue (self, content):
+		integer = 0
+		for i in range (0, len(content)):
+			integer += int(content[i]) << (8*i)
+		return integer
 		
 """
 [Bloc des données]
-DataBlocID      (4 octets) : Constante « data »  (0x64,0x61,0x74,0x61)
-DataSize        (4 octets) : Nombre d'octets des données (c.-à-d. "Data[]", c.-à-d. taille_du_fichier - taille_de_l'entête  (qui fait 44 octets normalement).
 DATAS[] : [Octets du Sample 1 du Canal 1] [Octets du Sample 1 du Canal 2] [Octets du Sample 2 du Canal 1] [Octets du Sample 2 du Canal 2]
 
 * Les Canaux :
--- python/dsp/reverb-profile.py
+++ python/dsp/reverb-profile.py
@@ -2,21 +2,27 @@
 
 import sys
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 # api
 from audio.wav import *
 
-def load_profile (wav):
-	"""
-	Returns binary content
-	of given wav file
-	should be a properly sampled impulse response
-	"""
-	with open(wav, 'rb') as fd:
-		content = fd.read()
-	return content
-
 def main (argv):
-	wav = Wav('../data/reverb-profiles/Deep Space.wav')
+	#wav = Wav('../../data/reverb-profiles/Deep Space.wav')
+	#wav = Wav('../../data/reverb-profiles/Large Wide Echo Hall.wav')
+	#wav = Wav('../../data/reverb-profiles/Nice Drum Room.wav')
+	wav = Wav('../../data/reverb-profiles/St Nicolaes Church.wav')
+	data = wav.getData()
+
+	fig = plt.figure(1)
+	ax = fig.add_subplot(111)
+	ax.plot(data[0], 'x-', label='Stereo - left')
+	ax.plot(data[1], 'x-', label='Stereo - right')
+	ax.grid(True)
+	ax.legend(loc='best')
+	
+	plt.show()
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
