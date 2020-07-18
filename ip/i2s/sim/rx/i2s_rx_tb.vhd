1827714df6113a833f67cbc11d126be67cdff0fa
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2020-07-18 15:04:50 +0200

    i2s:rx update

--- ip/i2s/sim/rx/i2s_rx_tb.vhd
+++ ip/i2s/sim/rx/i2s_rx_tb.vhd
@@ -10,13 +10,13 @@ architecture rtl of i2s_rx_tb is
 
 	signal bit_count_reg: natural range 0 to 8-1 := 7;
 
-	signal i2s_bclk: std_logic := '0';
-	signal i2s_din, i2s_dout: std_logic := '0';
-	signal i2s_din_reg: std_logic := '0';
-	signal i2s_lr: std_logic := '0';
+	signal bclk: std_logic := '0';
+	signal din, i2s_dout: std_logic := '0';
+	signal din_reg: std_logic := '0';
+	signal lr: std_logic := '0';
 
-	signal i2s_lr_hold: std_logic := '0';
-	signal i2s_lr_hold_z1: std_logic := '0';
+	signal lr_hold: std_logic := '0';
+	signal lr_hold_z1: std_logic := '0';
 
 	type data_lut is array (0 to 1) of std_logic_vector(7 downto 0);
 	constant DIN_DATA_LUT: data_lut := (x"AA",x"55");
@@ -28,13 +28,13 @@ begin
 	
 	clk <= not(clk) after 5.0 ns;
 
-	i2s_bclk <= not(i2s_bclk) after 100.0 ns;
+	bclk <= not(bclk) after 100.0 ns;
 
-	fake_stereo_lr: process (i2s_bclk)
+	fake_stereo_lr: process (bclk)
 	begin
-	if falling_edge (i2s_bclk) then
+	if falling_edge (bclk) then
 		if bit_count_reg = 1 then
-			i2s_lr <= not(i2s_lr);
+			lr <= not(lr);
 		end if;
 		
 		if bit_count_reg = 0 then
@@ -44,34 +44,34 @@ begin
 		end if;
 
 		if is_odd = '1' then
-			if i2s_lr = '1' then
-				i2s_din <= DIN_DATA_LUT(0)(bit_count_reg);
+			if lr = '1' then
+				din <= DIN_DATA_LUT(0)(bit_count_reg);
 			else
-				i2s_din <= DIN_DATA_LUT(1)(bit_count_reg);
+				din <= DIN_DATA_LUT(1)(bit_count_reg);
 			end if;
 		else
-			if i2s_lr = '1' then
-				i2s_din <= DIN_DATA_LUT(1)(bit_count_reg);
+			if lr = '1' then
+				din <= DIN_DATA_LUT(1)(bit_count_reg);
 			else
-				i2s_din <= DIN_DATA_LUT(0)(bit_count_reg);
+				din <= DIN_DATA_LUT(0)(bit_count_reg);
 			end if;
 		end if;
 	end if;
 	end process;
 
-	process (i2s_bclk) 
+	process (bclk) 
 	begin
-	if rising_edge (i2s_bclk) then
-		i2s_din_reg <= i2s_din;
-		i2s_lr_hold <= i2s_lr;
+	if rising_edge (bclk) then
+		din_reg <= din;
+		lr_hold <= lr;
 	end if;
 	end process;
 
 	process (clk)
 	begin
 	if rising_edge (clk) then
-		i2s_lr_hold_z1 <= i2s_lr_hold;
-		if i2s_lr_hold = '0' and i2s_lr_hold_z1 = '1' then -- L+R done
+		lr_hold_z1 <= lr_hold;
+		if lr_hold = '0' and lr_hold_z1 = '1' then -- L+R done
 			is_odd <= not(is_odd);
 		end if;
 	end if;
@@ -86,9 +86,9 @@ begin
 		stereo_valid => stereo_out_valid,
 		stereo_data => stereo_out_data,
 		-- I2S
-		i2s_bclk => i2s_bclk,
-		i2s_din => i2s_din_reg,
-		i2s_lr => i2s_lr
+		bclk => bclk,
+		din => din_reg,
+		lr => lr
 	);
 
 end rtl;
