62d04e5ec3f5bd5abc009095d1d92482dac5c573
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2020-07-24 12:41:13 +0200

    i2S rx resync

--- ip/i2s/src/i2s_rx.vhd
+++ ip/i2s/src/i2s_rx.vhd
@@ -23,6 +23,14 @@ end i2s_rx;
 
 architecture rtl of i2s_rx is
 
+	signal bclk_rsync_reg: std_logic_vector(2 downto 0) := (others => '0');
+	signal din_rsync_reg: std_logic_vector(2 downto 0) := (others => '0');
+	signal lr_rsync_reg: std_logic_vector(2 downto 0) := (others => '0');
+
+	signal bclk_rsync: std_logic;
+	signal din_rsync: std_logic;
+	signal lr_rsync: std_logic;
+
 	signal bclk_z1_s: std_logic := '0';
 	signal bclk_rising_s: std_logic;
 
@@ -34,6 +42,22 @@ architecture rtl of i2s_rx is
 	signal bit_count_reg: natural range 0 to G_DATA_WIDTH-1 := G_DATA_WIDTH-1;
 	signal data_r_reg, data_l_reg: std_logic_vector(G_DATA_WIDTH-1 downto 0) := (others => '0');
 begin
+
+	----------------------------
+	-- I2S => sys_clk resync.
+	----------------------------
+	process (clk)
+	begin
+	if rising_edge (clk) then
+		bclk_rsync_reg <= bclk_rsync_reg(bclk_rsync_reg'high-1 downto 0) & bclk;
+		din_rsync_reg <= din_rsync_reg(din_rsync_reg'high-1 downto 0) & din;
+		lr_rsync_reg <= lr_rsync_reg(lr_rsync_reg'high-1 downto 0) & lr;
+	end if;
+	end process;
+
+	bclk_rsync <= bclk_rsync_reg(bclk_rsync_reg'high);
+	din_rsync <= din_rsync_reg(din_rsync_reg'high);
+	lr_rsync <= lr_rsync_reg(lr_rsync_reg'high);
 	
 	----------------------------
 	-- BCLK rising edge detector
@@ -42,8 +66,8 @@ begin
 	begin
 	if rising_edge (clk) then
 		bclk_rising_s <= '0';
-		bclk_z1_s <= bclk;
-		if bclk = '1' and bclk_z1_s = '0' then
+		bclk_z1_s <= bclk_rsync;
+		if bclk_rsync = '1' and bclk_z1_s = '0' then
 			bclk_rising_s <= '1';
 		end if;
 	end if;
@@ -56,7 +80,7 @@ begin
 	begin
 	if rising_edge (clk) then
 		if bclk_rising_s = '1' then
-			lr_delayed_s <= lr;
+			lr_delayed_s <= lr_rsync;
 		end if;
 
 		lr_delayed_z1_s <= lr_delayed_s;
@@ -84,9 +108,9 @@ begin
 		if bclk_rising_s = '1' then -- sampling on rising edge
 			-- stereo sampling
 			if lr_delayed_s = '1' then
-				data_r_reg(bit_count_reg) <= din;
+				data_r_reg(bit_count_reg) <= din_rsync;
 			else
-				data_l_reg(bit_count_reg) <= din;
+				data_l_reg(bit_count_reg) <= din_rsync;
 			end if;
 
 			-- pointer / MSBF
